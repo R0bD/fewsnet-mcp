0a7b93e2dd21f3c10b25a1ccc0e1ece20a63e5a8
Rob Dickens <robert.dickens@live.co.uk>
2025-11-12 00:11:02 +0000

    remove async

--- main.py
+++ main.py
@@ -13,38 +13,38 @@ API_BASE_URL = 'https://fdw.fews.net/api'
 AUTH_ENDPOINT = 'https://fdw.fews.net/api-token-auth/'
 
 
-async def get_config() -> Dict[str, str]:
+def get_config() -> Dict[str, str]:
     return {
         "username": os.environ.get("username", ""),
         "password": os.environ.get("password", "")
     }
 
 
-async def get_token() -> str:
+def get_token() -> str:
     data = get_config()
-    async with httpx.AsyncClient() as client:
-        response = await client.post(AUTH_ENDPOINT, data=data, timeout=30.0)
+    with httpx.AsyncClient() as client:
+        response = client.post(AUTH_ENDPOINT, data=data, timeout=30.0)
         response.raise_for_status()
         return response.json()['token']
 
 
-async def get_headers() -> Dict[str, str]:
-    token = await get_token()
+def get_headers() -> Dict[str, str]:
+    token = get_token()
     return {
         "Authorization": f"JWT {token}"
     }
 
 
-async def make_request(url: str, country_code: str, start_date: str) -> str:
+def make_request(url: str, country_code: str, start_date: str) -> str:
     params = {
         "country_code": country_code,
         "start_date": start_date
     }
-    headers = await get_headers()
+    headers = get_headers()
 
-    async with httpx.AsyncClient() as client:
+    with httpx.AsyncClient() as client:
         try:
-            response = await client.get(url, params=params, headers=headers, timeout=30.0)
+            response = client.get(url, params=params, headers=headers, timeout=30.0)
             response.raise_for_status()
             logging.debug(response.json())
             text = response.text
@@ -58,40 +58,40 @@ async def make_request(url: str, country_code: str, start_date: str) -> str:
     name="get_market_price_facts",
     description="get time series market prices for specific products from hte United Nations Central Product "
                 "Classification")
-async def get_market_price_facts(country_code: str, start_date: str) -> str:
+def get_market_price_facts(country_code: str, start_date: str) -> str:
 
     url = f"{API_BASE_URL}/marketpricefacts"
-    return await make_request(url=url, country_code=country_code, start_date=start_date)
+    return make_request(url=url, country_code=country_code, start_date=start_date)
 
 
 @mcp.tool(
     name="get_cross_border_trade",
     description="get cross border trade time series data for specific products from hte United Nations Central Product "
                 "Classification")
-async def get_cross_border_trade(country_code: str, start_date: str) -> str:
+def get_cross_border_trade(country_code: str, start_date: str) -> str:
 
     url = f"{API_BASE_URL}/tradeflowquantityvalue"
-    return await make_request(url=url, country_code=country_code, start_date=start_date)
+    return make_request(url=url, country_code=country_code, start_date=start_date)
 
 
 @mcp.tool(
     name="get_fews_net_food_security_classification",
     description="Classification and series catalog data"
 )
-async def get_fews_net_food_security_classification(country_code: str, start_date: str) -> str:
+def get_fews_net_food_security_classification(country_code: str, start_date: str) -> str:
 
     url = f"{API_BASE_URL}/ipcclassification"
-    return await make_request(url=url, country_code=country_code, start_date=start_date)
+    return make_request(url=url, country_code=country_code, start_date=start_date)
 
 
 @mcp.tool(
     name="get_food_insecure_population_estimates",
     description="get data on food insecure population estimates"
 )
-async def get_food_insecure_population_estimates(country_code: str, start_date: str) -> str:
+def get_food_insecure_population_estimates(country_code: str, start_date: str) -> str:
 
     url = f"{API_BASE_URL}/ipcpopulation"
-    return await make_request(url=url, country_code=country_code, start_date=start_date)
+    return make_request(url=url, country_code=country_code, start_date=start_date)
 
 
 def main():
